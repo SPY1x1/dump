e8b426d769816c2f400cc0a022274def1ef88855
SAT Shahin YT <109285746+SPY1x1@users.noreply.github.com>
2023-03-23 23:52:58 +0600

    Update spy.py

--- spy.py
+++ spy.py
@@ -4,7 +4,7 @@ os.system('clear')
 os.system("git pull")
 b = platform.architecture()[0]
 if b == '64bit':
-    __import__("VIP").Spy()
+    __import__("DUMP").Spy()
     
 elif b == '32bit':
     print(" your phone is 32 bit \n 32 bit comming soon")
